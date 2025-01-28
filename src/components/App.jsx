fa1e74121d1d7b5579c855eaa7531899dddb9d69
jessasesh <jessica.dison@yahoo.com>
2025-01-28 17:59:45 -0600

    Separated footer from music player; added dark mode to entire background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,13 @@ import MusicPlayer from "./MusicPlayer";
 
 function App() {
   return (
-    <div className="h-full flex flex-col justify-between p-8 min-h-screen">
-      <MusicPlayer />
-      <Footer />
+    <div className="bg-background dark:bg-darkBackground h-full flex flex-col justify-between p-8 min-h-screen">
+      <div className=" sm:flex justify-between p-8">
+        <MusicPlayer />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
