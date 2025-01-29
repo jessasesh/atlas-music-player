d79479855acbe76a456eb96e9c4d54796dd8c6df
jessasesh <jessica.dison@yahoo.com>
2025-01-28 18:06:19 -0600

    Added coverart component to display cover image

---
+++ src/components/CoverArt.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import coverImage from "/images/cover-art.png";
+
+const CoverArt = () => {
+    return (
+        <div className="">
+            <img className="" src= {coverImage} ></img>
+        </div>
+    );
+};
+
+export default CoverArt;
