909088df2fd934e8869841480fd6984b711542c0
jessasesh <jessica.dison@yahoo.com>
2025-01-28 18:19:04 -0600

    Imported both currently playing and playlist items to bring the entire music player together

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,3 +1,14 @@
-export default function MusicPlayer() {
-  return <p>Music player goes here</p>;
-}
+import React from "react";
+import CurrentlyPlaying from "./CurrentlyPlaying";
+import Playlist from "./Playlist";
+
+const MusicPlayer = () => {
+  return (
+    <div className="sm:flex">
+        <CurrentlyPlaying />
+        <Playlist />
+    </div>
+  );
+};
+
+export default MusicPlayer;
