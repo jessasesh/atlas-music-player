81f29f0deb846b6625dd3682aec1c146d9dbd3ad
jessasesh <jessica.dison@yahoo.com>
2025-01-28 18:21:59 -0600

    Implemented darkmode to match user's system preference; added custom theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,18 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#f4f7fa',
+        textPrimary: '#2d3748',
+
+        darkBackground: '#1a202c',
+        darkTextPrimary: '#e2e8f0',
+        highlight: '#e2e8f0',
+      },
+    },
   },
   plugins: [],
 }
