edab20f99d4d4c5db7a09064d09cac3342df1a49
jessasesh <jessica.dison@yahoo.com>
2025-01-28 18:12:57 -0600

    Wasn't sure if we were supposed to code the range slider, have it disabled, or just use the img; went with image and will work out next assignment

---
+++ src/components/VolumeControls.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const VolumeControls = () => {
+    return (
+        <div>
+            <img className="mt-4 rounded-lg" src="/images/volume-control.png"></img>
+        </div>
+    );
+};
+
+export default VolumeControls;
