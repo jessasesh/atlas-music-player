1cd0a4f6e24cd13a47c07e478b72103676938448
jessasesh <jessica.dison@yahoo.com>
2025-01-29 20:54:50 -0600

    Changed file type from JSX to TSX

--- src/components/Footer.jsx
+++
@@ -1,4 +0,0 @@
-export default function Footer() {
-  const year = new Date().getFullYear();
-  return <div className="text-center p-8 text-textPrimary dark:text-darkTextPrimary">&copy; {year} Atlas School</div>;
-}
