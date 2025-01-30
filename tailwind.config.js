3c606adfe443e2d2610f67dbdf6b36963566749a
jessasesh <jessica.dison@yahoo.com>
2025-01-29 20:47:02 -0600

    Reworkedd the theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: '#f4f7fa',
-        textPrimary: '#2d3748',
-
-        darkBackground: '#1a202c',
-        darkTextPrimary: '#e2e8f0',
-        highlight: '#e2e8f0',
+        background: "#f4f7fa",
+        textPrimary: "#2d3748",
+        cardBg: "#ffffff",
+        highlight: "#e2e8f0",
+        darkBackground: "#1a202c",
+        darkTextPrimary: "#e2e8f0",
+        darkCardBg: "#2d3748",
+        darkHighlight: "#4a5568",
+        primary: "#6B8E23",
       },
     },
   },
